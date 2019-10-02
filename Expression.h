27eb13dfdb642919f22cf52d565f1ed48ae2aad8
Sergey Kozharinov <hensuto@gmail.com>
2019-10-02 18:08:02 +0300

    resolve merge conflicts

--- Expression.h
+++ Expression.h
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-#ifndef EXPRESSION_H
-#define EXPRESSION_H
-
-#include "HashMap.h"
-#include "List.h"
-#include "Queue.h"
-#include "mystring.h"
-
-class Expression {
-public:
-  using ResultType = double;
-  enum BraceType { BraceType_Round, BraceType_Square, BraceType_Unknown };
-  enum Associativity {
-    Associativity_Left,
-    Associativity_Right,
-    Associativity_Unknown
-  };
-  explicit Expression(Queue<MyString>);
-  Expression(const Expression &);
-  ResultType evaluate() const;
-  Queue<MyString> getPostfix() const;
-  List<MyString> getVariablesNames() const;
-  ResultType getVariable(MyString &) const;
-  void setVariable(MyString &, ResultType);
-  static bool isNumber(const MyString &);
-  static bool isOperator(const MyString &);
-  static bool isUnaryOperator(const MyString &);
-  static bool isBinaryOperator(const MyString &);
-  static bool isFunction(const MyString &);
-  static bool isUnaryFunction(const MyString &);
-  static bool isBinaryFunction(const MyString &);
-  static bool isConstant(const MyString &);
-  static bool isLeftBrace(const MyString &);
-  static bool isRightBrace(const MyString &);
-  static bool isComma(const MyString &);
-  static bool isVariable(const MyString &);
-  static int precedence(const MyString &);
-  static BraceType braceType(const MyString &);
-  static Associativity associativity(const MyString &);
-  static ResultType getConstant(const MyString &);
-  static ResultType doUnaryOperation(const MyString &, ResultType);
-  static ResultType doBinaryOperation(const MyString &, ResultType, ResultType);
-  static ResultType parseNumber(const MyString &);
-
-private:
-  Queue<MyString> postfix;
-  HashMap<MyString, ResultType> variables;
-  static void failWrongToken(const MyString &);
-  static void failNotANumber(const MyString &);
-  static void failNotAConstant(const MyString &);
-  static void failNotABrace(const MyString &);
-  static void failNotAnUnaryOperator(const MyString &);
-  static void failNotABinaryOperator(const MyString &);
-  static void failWrongAssociativity(const MyString &);
-  static void failWrongBraceType(const MyString &);
-  static void failMissingLeftBrace();
-  static void failMissingRightBrace();
-  static void failMissingOperand();
-  static void failMissingOperator();
-};
-
-#endif
-=======
 #ifndef EXPRESSION_H
 #define EXPRESSION_H
 
@@ -119,4 +55,3 @@ private:
 };
 
 #endif
->>>>>>> cc4f7a71590d8d6f35516dc65ef1573b9b2a42e3
--- Math.h
+++ Math.h
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-#ifndef MATH_H
-#define MATH_H
-
-class Math {
-public:
-  static const double Epsilon;
-  static const double NotANumber;
-  static const double PositiveInfinity;
-  static const double NegativeInfinity;
-  static const double Pi;
-  static const double Euler;
-  static double sin(double);
-  static double cos(double);
-  static double log(double, double);
-  static double log(double);
-  static double exp(double);
-  static double pow(double, double);
-  static double abs(double);
-  static double sqrt(double);
-  static double cbrt(double);
-  static double min(double, double);
-  static double max(double, double);
-
-private:
-  static double reduce2Pi(double);
-};
-
-#endif
-=======
 #ifndef MATH_H
 #define MATH_H
 
@@ -52,4 +22,3 @@ Number max(const Number &, const Number &);
 } // namespace Math
 
 #endif
->>>>>>> cc4f7a71590d8d6f35516dc65ef1573b9b2a42e3
